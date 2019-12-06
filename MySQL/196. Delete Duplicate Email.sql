3875621a85313467c6fc4f2826b44514c1a4acc1
irsol <irinywka@gmail.com>
2019-12-06 14:54:16 +0100

    Updated file

--- MySQL/196. Delete Duplicate Email.sql
+++ MySQL/196. Delete Duplicate Email.sql
@@ -17,3 +17,9 @@
 # | 1  | john@example.com |
 # | 2  | bob@example.com  |
 # +----+------------------+
+#
+# Write your MySQL query statement below
+
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.id > p2.id and p1.email = p2.email;
