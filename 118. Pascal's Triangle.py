4f9714aabdaa9168eae570eae484690e2a400d10
irsol <irinywka@gmail.com>
2020-01-19 21:11:40 +0100

    Added solution

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -7,3 +7,23 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
+        output = []
+
+        for r in range(numRows):
+            row = [1] * (r + 1)
+            for c in range(1, r):
+                
+                row[c] = output[r - 1][c -1] + output[r -1][c]
+            output.append(row)
+        return output 
+
+
+      """
+        output = []
+        for row in range(numRows):
+            output.append(1)
+
+            for i in range(row - 1, 0, -1):
+              output[i] += output[i - 1]
+            print(*output)
+      """
