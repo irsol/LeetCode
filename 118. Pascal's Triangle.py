57561fa1e6a6d8f879b8df351104940339903f77
irsol <irinywka@gmail.com>
2020-01-19 12:21:17 +0100

    Added problem 118. Pascal's Triangle

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,9 @@
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
