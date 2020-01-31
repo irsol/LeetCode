0463b62264f0d9681fd0acdc4aed0187bdb5d4b3
irsol <irinywka@gmail.com>
2020-01-31 13:03:30 +0100

    Added solution

--- 119. Pascal's Triangle2.py
+++ 119. Pascal's Triangle2.py
@@ -7,9 +7,17 @@
 # Input: 3
 # Output: [1,3,3,1]
 
+# Runtime: 28 ms, faster than 11.28% of Python online submissions for Pascal's Triangle II.
+# Memory Usage: 11.8 MB, less than 26.92% of Python online submissions for Pascal's Triangle II.
+
 class Solution(object):
     def getRow(self, rowIndex):
         """
         :type rowIndex: int
         :rtype: List[int]
         """
+        row = [1] * (rowIndex + 1)
+        for x in range(1, rowIndex + 1):
+          for y in range(x - 1, 0, -1):
+            row[y] += row[y - 1]
+        return row  
