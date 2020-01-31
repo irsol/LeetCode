fdf0907888547350c038c632a8442213cbfd8445
irsol <irinywka@gmail.com>
2020-01-31 12:33:55 +0100

    Added description

---
+++ 119. Pascal's Triangle2.py
@@ -0,0 +1,15 @@
+# Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+# Note that the row index starts from 0.
+
+# Example:
+
+# Input: 3
+# Output: [1,3,3,1]
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
