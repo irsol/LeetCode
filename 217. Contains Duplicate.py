daf9fcc87d3044e98a2dcc9753c834f3cb5c0a61
irsol <irinywka@gmail.com>
2020-03-28 11:54:01 +0100

    Added solution

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -4,6 +4,15 @@
 # Your function should return true if any value appears at least twice in
 # the array, and it should return false if every element is distinct.
 
+# Runtime: 100 ms, faster than 91.41% of Python online submissions for Contains Duplicate.
+# Memory Usage: 18 MB, less than 5.55% of Python online submissions for Contains Duplicate.
+
 class Solution(object):
     def containsDuplicate(self, nums):
-        
+        nums_set = set()
+
+        for x in nums:
+            if x in nums_set:
+                return True
+            nums_set.add(x)            
+        return False
